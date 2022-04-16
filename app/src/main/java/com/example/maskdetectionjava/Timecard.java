059fd6cd22b3491da9b40e434f7435f3e6879d19
delizcolonj <delizcolonj@wit.edu>
2022-04-16 16:06:48 -0400

    timecard update

--- app/src/main/java/com/example/maskdetectionjava/Timecard.java
+++ app/src/main/java/com/example/maskdetectionjava/Timecard.java
@@ -1,4 +1,30 @@
 package com.example.maskdetectionjava;
 
-public class Timecard {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Timecard extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timecard_layout);
+
+
+        //initiate textviews
+        TextView title = findViewById(R.id.Maintitle);
+        TextView monTitle = findViewById(R.id.SubtitleMon);
+        TextView tuesTitle = findViewById(R.id.SubTitleTues);
+        TextView wedTitle = findViewById(R.id.SubTitleWed);
+        TextView thursTitle = findViewById(R.id.SubTitleThur);
+        TextView friTitle = findViewById(R.id.SubTitleFri);
+        TextView satTitle = findViewById(R.id.SubTitleSat);
+        TextView sunTitle = findViewById(R.id.SubTitleSun);
+
+        //initiate edittime
+
+    }
 }
